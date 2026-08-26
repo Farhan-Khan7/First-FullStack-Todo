3f7481ecc5362c70e636f9abbab8cb23f0ba2273
Farhan-Khan7 <fk5518744@gmail.com>
2026-08-26 19:27:09 +0530

    fix cors and url

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://fullstack-todo-k50r.onrender.com",
+      "https://fullstack-todo-991a.onrender.com",
     ],
     credentials: true,
   }),
