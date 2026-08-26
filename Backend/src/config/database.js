c3e74d76e5f62fd1292358c107ea595534628b76
Farhan-Khan7 <fk5518744@gmail.com>
2026-08-26 17:09:20 +0530

    Finally fixed my backend database code

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
 
-function connectDB(){
-    mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("Successfully Connected with Database!"))
-}
+function connectDB() {
 
+    mongoose.connect(process.env.MONGO_URI)
+
+        // Agar MongoDB successfully connect ho gaya
+        .then(() => {
+            console.log("Successfully Connected with Database!")
+        })
+
+        // Agar connection fail ho gaya
+        .catch((error) => {
+            console.log("MongoDB Connection Error:", error)
+        })
+}
 
 module.exports = connectDB
